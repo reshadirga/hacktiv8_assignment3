8dfc68aa364f68be628870294b4ba6a2e88d230b
reshadirga <official.reshadirga@gmail.com>
2021-02-19 23:39:16 +0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import { Table, Row, Col, Space, Card } from 'antd';
+import { Get, withAxios } from 'react-axios';
+import { useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const dataSource = [
+  {
+    key: '1',
+    name: 'Mike',
+    age: 32,
+    address: '10 Downing Street',
+  },
+  {
+    key: '2',
+    name: 'John',
+    age: 42,
+    address: '10 Downing Street',
+  },
+];
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+];
+
+const TableApp = () => {
+
+  useEffect(() => {
+    axios.get("https://api.exchangeratesapi.io/latest").then((res) => {
+    console.log('res', res.data.base);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Row>
+      <Col lg={{ span: 12, offset: 2 }}>
+        <Space direction="vertical">
+          <Card title="Table Chart Stock" style={{ width: 400 }}>
+            <Table dataSource={dataSource} columns={columns} />
+          </Card>
+        </Space>
+      </Col>
+    </Row>
   );
 }
 
-export default App;
+export default TableApp;
