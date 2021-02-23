36a713d544e2a48d98b578218c35b6df4caa9907
reshadirga <official.reshadirga@gmail.com>
2021-02-24 00:51:47 +0700

    Formatting and add input box

--- src/App.js
+++ src/App.js
@@ -1,79 +1,140 @@
-import axios from "axios";
-import { Row, Col, Space, Table, Card } from "antd";
-import { useEffect } from "react";
+import logo from './logo.svg';
 import './App.css';
- 
-const dataSource = [
-      {
-        key: '1',
-        currency: 'CAD',
-        buy: 10,
-        exchangeRate: 50,
-        sell: 100,
-      },
-      {
-        key: '2',
-        currency: 'IDR',
-        buy: 10,
-        exchangeRate: 50,
-        sell: 100,
-      },
-      {
-        key: '3',
-        currency: 'JPY',
-        buy: 10,
-        exchangeRate: 50,
-        sell: 100,
-      },
-      {
-        key: '4',
-        currency: 'CHF',
-        buy: 10,
-        exchangeRate: 50,
-        sell: 100,
-      },
-    ];
- 
-const columns = [
-      {
-        title: '',
-        dataIndex: 'currency',
-      },
-      {
-        title: 'WE BUY',
-        dataIndex: 'buy',
-      },
-      {
-        title: 'EXCHANGE RATE',
-        dataIndex: 'exchangeRate',
-      },
-      {
-        title: 'WE SELL',
-        dataIndex: 'sell',
-      },
-    ];
- 
+import { Table, Row, Col, Space, Card, Input, Select } from 'antd';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Moment from 'react-moment';
+
 const TableApp = () => {
+
+  const [baseCurrencySource, setBaseCurrencySource] = useState([]);
+  const [dateSource, setDateSource] = useState([]);
+  const [dataSource, setDataSource] = useState([]);
+  const [baseCurrencyName, setBaseCurrencyName] = useState([]);
+  const columns = [
+    {
+      title: '',
+      dataIndex: 'currency',
+      key: 'currency',
+    },
+    {
+      title: 'We Buy',
+      dataIndex: 'weBuy',
+      key: 'weBuy',
+    },
+    {
+      title: 'Exchange Rate',
+      dataIndex: 'exchangeRate',
+      key: 'exchangeRate',
+    },
+    {
+      title: 'We Sell',
+      dataIndex: 'weSell',
+      key: 'weSell',
+    },
+  ];
+
+  const { Option } = Select;
+
+  
+
   useEffect(() => {
- 
-    (async() => {
-      const result = await axios.get("https://api.exchangeratesapi.io/latest");
-      console.log('result',result);
-      })();
-    }, []);
- 
- 
-    return (
+    axios.get("https://api.exchangeratesapi.io/latest").then((res) => {
+
+      if (res.status !== 200) return;
+      else {
+        setBaseCurrencySource(res.data?.base);
+        setDateSource(res.data?.date);
+        setDataSource(setDataToTableFormat(res.data?.rates));
+        setBaseCurrencyName(baseCurrencyNameOptions(res.data?.rates));
+      }
+
+    });
+  }, []);
+
+  return (
     <Row>
-      <Col lg={{ span: 12, offset: 2 }}>
+      <Col md={{ span: 12, offset: 6 }}>
         <Space direction="vertical">
-          <Card title="Table Chart Stock" style={{ width: 900 }}>
-            <Table dataSource={dataSource} columns={columns} />
+          <Card title="Table Chart Stock" style={{ width: 600 }}>
+            <Input addonBefore={baseCurrencyName} defaultValue="Base currency value..." />
+            <div className="infoHeader">
+              <Col sm={{ span: 15, offset: 0 }}>
+                <h3>Base currency: {baseCurrencySource}</h3>
+              </Col>
+              <Col sm={{ span: 9, offset: 0 }}>
+                <p>Date:{" "}
+                  <Moment parse="YYYY-MM-DD" format="DD MMMM YYYY">
+                    {dateSource}
+                  </Moment>
+                </p>
+              </Col>
+            </div>
+            <div className="currencyTable">
+              <Table dataSource={dataSource} columns={columns} />
+            </div>
           </Card>
         </Space>
       </Col>
     </Row>
+
   );
-};
- 
+
+}
+const baseCurrencyNameOptions = (dataSource) => {
+
+  let options = [];
+
+  for (const key in dataSource) options.push(
+    <Option value={key}>{key}</Option>
+  )
+  // console.log(dataSource)
+
+  return (
+    <Select defaultValue="IDR" className="select-before">
+      { options }
+    </Select>
+  )
+
+}
+
+const setDataToTableFormat = (rates) => {
+
+  let dataSource = [];
+  let index = 0;
+
+  for (const key in rates) {
+    let value = rates[key];
+    let readForexObject =
+    {
+      key: index++,
+      currency: key,
+      weBuy: formatCurrencyRate(value - spreadPrice(value)),
+      exchangeRate: formatCurrencyRate(value),
+      weSell: formatCurrencyRate(value + spreadPrice(value)),
+    };
+
+    dataSource.push(readForexObject);
+
+  }
+
+  return dataSource;
+}
+
+const spreadPrice = (exchangeRate) => {
+  return exchangeRate * 0.1;
+}
+
+const formatCurrencyRate = (rates) => {
+  let value = Math.round(rates * 1000) / 1000;
+  // let intValue = Math.round(rates);
+  // let intValueDivider = 0;
+
+  // if (intValue > 1000) intValueDivider = (Math.log10)/3;
+  // for (let i = 0; i < intValueDivider; i++)
+
+
+  return value;
+}
+
 export default TableApp;
