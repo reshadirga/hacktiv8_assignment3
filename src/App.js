8037d45ba7ab99fdfbd6188da861d2d5f8256da1
reshadirga <official.reshadirga@gmail.com>
2021-02-24 23:13:01 +0700

    Input section is functional

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Table, Row, Col, Space, Card, Input, Select } from 'antd';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Moment from 'react-moment';
+import { string } from 'prop-types';
 
 const TableApp = () => {
 
@@ -35,8 +36,7 @@ const TableApp = () => {
   ];
 
   const { Option } = Select;
-
-  
+  const [inputValue, setInputValue] = useState('');
 
   useEffect(() => {
     axios.get("https://api.exchangeratesapi.io/latest").then((res) => {
@@ -57,7 +57,14 @@ const TableApp = () => {
       <Col md={{ span: 12, offset: 6 }}>
         <Space direction="vertical">
           <Card title="Table Chart Stock" style={{ width: 600 }}>
-            <Input addonBefore={baseCurrencyName} defaultValue="Base currency value..." />
+            <Input
+              className="baseCurrencyInput"
+              addonBefore={baseCurrencyName}
+              placeholder="Base currency value..."
+              allowClear="true"
+              value={inputValue}
+              onChange={e => setInputValue(formatNumber(e.target.value))}
+            />
             <div className="infoHeader">
               <Col sm={{ span: 15, offset: 0 }}>
                 <h3>Base currency: {baseCurrencySource}</h3>
@@ -84,11 +91,9 @@ const TableApp = () => {
 const baseCurrencyNameOptions = (dataSource) => {
 
   let options = [];
-
   for (const key in dataSource) options.push(
     <Option value={key}>{key}</Option>
   )
-  // console.log(dataSource)
 
   return (
     <Select defaultValue="IDR" className="select-before">
@@ -113,11 +118,8 @@ const setDataToTableFormat = (rates) => {
       exchangeRate: formatCurrencyRate(value),
       weSell: formatCurrencyRate(value + spreadPrice(value)),
     };
-
     dataSource.push(readForexObject);
-
   }
-
   return dataSource;
 }
 
@@ -127,14 +129,67 @@ const spreadPrice = (exchangeRate) => {
 
 const formatCurrencyRate = (rates) => {
   let value = Math.round(rates * 1000) / 1000;
-  // let intValue = Math.round(rates);
-  // let intValueDivider = 0;
-
-  // if (intValue > 1000) intValueDivider = (Math.log10)/3;
-  // for (let i = 0; i < intValueDivider; i++)
-
-
   return value;
+}
+
+function formatNumber(value) {
+
+  if (checkNumberValidity(value)) return alertInputTypeError(value);
+
+  if (value < 1000) return value;
+  else {
+    const stringValue = value.toString().split('');
+    let decimalValue = [];
+    let stringDecimalValue = '';
+    let onDecimal = false;
+    let convertedValue = '';
+
+    stringValue.map((v, i) => {
+      if (v == ",") stringValue.splice(i, 1);
+      if (v == "." || onDecimal) {
+        onDecimal = true;
+        decimalValue.push(v);
+      }
+      return stringValue;
+    })
+
+    for (let i = 0; i < decimalValue.length; i++) stringValue.pop();
+
+    for (let i = stringValue.length - 1; i >= 0; i--) {
+      let digits = stringValue.length - i;
+      if (digits > 2 && digits % 3 == 1) convertedValue = stringValue[i] + "," + convertedValue
+      else convertedValue = stringValue[i] + convertedValue;
+    }
+
+    decimalValue.map((v) => stringDecimalValue += v);
+    if (onDecimal) convertedValue += stringDecimalValue;
+
+    return convertedValue;
+  }
+}
+
+const alertInputTypeError = (value) => {
+  alert('Please enter number only');
+
+  let cleanedValue = '';
+  const inputs = value.split('');
+
+  inputs.pop();
+  inputs.map((v) => cleanedValue += v);
+
+  return cleanedValue;
+}
+
+const checkNumberValidity = (value) => {
+
+  const inputs = value.split('');
+  let check = false;
+
+  inputs.map((v) => {
+    if (v != "1" && v != "2" && v != "3" && v != "4" && v != "5" && v != "6" && v != "7" && v != "8" && v != "9" && v != "0" && v != "," && v != ".") check = true;
+    else check = false;
+  })
+  return check;
 }
 
 export default TableApp;
